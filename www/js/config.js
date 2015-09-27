052a17c62282c8c175b3bb98d518c8fd2c8d18c0
laihui0207 <laihui0207gm@gmail.com>
2015-09-28 00:13:53 +0800

    progress complete

--- www/js/config.js
+++ www/js/config.js
@@ -4,9 +4,9 @@
 angular.module("Holu.config", [])
     .constant("ENV", {
         "accessToken": '',
-/*         ServerUrl: "http://220.178.1.10:8089/holusystem",*/
+         ServerUrl: "http://220.178.1.10:8089/holusystem",
 /*        ServerUrl: "http://localhost:8087/holusystem",*/
-        ServerUrl: "http://192.168.199.162:8087/holusystem",
+/*        ServerUrl: "http://192.168.199.162:8087/holusystem",*/
 /*        ServerUrl: "http://10.140.200.38:8087/holusystem",*/
         "debug": false,
         'version':'1.0.0-beta'
